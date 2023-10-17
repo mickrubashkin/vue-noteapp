ef2d98d00a65c93f486e24e020e4546b5d20d9c8
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-10-17 16:37:47 +0200

    🏁

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="notes">
+
+    <div class="card has-background-success-dark p-4 mb-5">
+
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right ">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
+
+    <div v-for="i in 3" class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius temporibus consectetur tempora, libero mollitia
+          quidem expedita, culpa similique enim earum accusamus! Dignissimos fugiat sequi provident voluptatem saepe
+          numquam autem quaerat?
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
